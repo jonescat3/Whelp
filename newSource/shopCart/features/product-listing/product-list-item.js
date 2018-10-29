bd7cc99144ef481d3a66269da73013eab1ebbbe4
Trina Raye <jonescatrina1@gmail.com>
2018-10-28 23:57:17 -0400

    updates

---
+++ newSource/shopCart/features/product-listing/product-list-item.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import AddBtn from './add-btn'
+import RemoveBtn from './remove-btn'
+
+export default function ProuctListItem(props) {
+    return <div className='product-list-item'>
+        <h3>{ props.name }</h3> 
+
+    <div>{props.product.description }</div>
+       <div>${ props.product.price }</div>
+    <div>
+      <AddBtn 
+        cartItem={props.cartItem} 
+        product={props.product} 
+        addToCart={props.addToCard}
+    />
+
+    {
+        props.cartItem
+            ? <RemoveBtn 
+                cartItem={props.cartItem} 
+                product={props.product} 
+                addToCart={props.addToCard}
+                removeFromCart={props.removeFromCart}
+            />
+            : null
+    }
+
+        </div>
+    </div>
+}
